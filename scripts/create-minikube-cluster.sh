a2765e969db3921ad73c2561d50b607dd50bef74
stev <stefano.valladares@akamas.io>
2021-10-01 09:32:52 +0200

    Fixed creation of minikube cluster

--- scripts/create-minikube-cluster.sh
+++ scripts/create-minikube-cluster.sh
@@ -2,15 +2,12 @@
 
 set -e
 
+HOST_IP="$1"
+
 echo "Setting up docker for Minikube"
 sudo chown -R "$USER":"$USER" ~/.docker/
 
-if ! command -v dig &> /dev/null; then
-  sudo apt install dnsutils
-fi
-
-HOST_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-minikube start --cpus=4 --memory=8g --ports=30900:30900,30899:30899,30800:30800,8999:8999 --apiserver-port=8999 --apiserver-ips="$HOST_IP"
+minikube start --apiserver-port=8999 --apiserver-ips="$HOST_IP" --cpus=4 --memory=8g --ports=30900:30900,30899:30899,30800:30800,8999:8999 --namespace=akamas-demo
 
 cp ~/.minikube/profiles/minikube/client.crt /akamas-config/
 cp ~/.minikube/profiles/minikube/client.key /akamas-config/
@@ -22,7 +19,6 @@ sed -i -r 's/client-certificate:.*/client-certificate: \/kubeconfig\/client\.crt
 sed -i -r 's/client-key:.*/client-key: \/kubeconfig\/client\.key/g' /akamas-config/config
 sed -i "s/server:.*:\(\.*\)/server: https:\/\/$HOST_IP:\1/g" /akamas-config/config
 
-kubectl label node minikube akamas/node=akamas
-kubectl config set-context --current --namespace=akamas-demo
+kubectl label node --all akamas/node=akamas
 
-echo "Cluster created. This is your CLUSTER_HOST_IP: $HOST_IP"
+echo "Cluster created"
