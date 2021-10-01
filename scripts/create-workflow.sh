7bb56687db7e91cdb8c803d45a1931d41e054b76
stev <stefano.valladares@akamas.io>
2021-10-01 10:23:50 +0200

    Fixed paths in scripts

--- scripts/create-workflow.sh
+++ scripts/create-workflow.sh
@@ -4,7 +4,7 @@ set -e
 
 CLUSTER_IP="$1"
 
-cd akamas/workflows 2>/dev/null || cd kubernetes-online-boutique/akamas/workflow || { echo "Run the script inside or one level above the kubernetes-online-boutique folder"; exit 1; }
+cd akamas/workflows 2>/dev/null || cd kubernetes-online-boutique/akamas/workflows || { echo "Run the script inside or one level above the kubernetes-online-boutique folder"; exit 1; }
 
 sed -i "s/CLUSTER_IP/$CLUSTER_IP/g" workflow.yaml
 
